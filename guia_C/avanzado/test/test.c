cc46cd5fe084a684a75884792cf3365bd9f1ef93
Tommy1908 <tommy.villafane3@gmail.com>
2025-09-03 15:31:34 -0300

    .

--- guia_C/avanzado/test/test.c
+++ guia_C/avanzado/test/test.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
 
-void allocateArray(int **arr, int size, int value)
-{
-	*arr = (int *)malloc(size * sizeof(int));
-	if (*arr != NULL)
-	{
-		for (int i = 0; i < size; i++)
-		{
-			(*arr)[i] = value;
-		}
-	}
-}
-// Uso
 
-int main()
-{
-	int *vector = NULL;
-	allocateArray(&vector, 5, 45);
-	for (int i = 0; i < 5; i++)
-		printf("%d\n", vector[i]);
-	free(vector);
+int main() {
+	int matrix[3][4] = {
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12}
+		};
+		
+	// p apunta al int en la fila 0, columna 0
+	int *p = &matrix[0][0];
+	
+	// ¿qué es reshape?
+	int (*reshape)[2] = (int (*)[2]) p;
+	
+	printf("%d\n", p[3]); // Qué imprime esta lı́nea?
+	printf("%d\n", reshape[1][1]); // Qué imprime esta lı́nea?
+	return 0;
 }
