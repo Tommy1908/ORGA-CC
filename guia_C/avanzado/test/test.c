2a3a590fff578fa4a6e9d522991949870a0b4ee8
Tommy1908_ <tommy.villafane3@gmail.com>
2025-09-04 00:51:23 -0300

    .

--- guia_C/avanzado/test/test.c
+++ guia_C/avanzado/test/test.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 
-
-int main() {
-	int matrix[3][4] = {
-		{1, 2, 3, 4},
-		{5, 6, 7, 8},
-		{9, 10, 11, 12}
-		};
-		
-	// p apunta al int en la fila 0, columna 0
-	int *p = &matrix[0][0];
-	
-	// ¿qué es reshape?
-	int (*reshape)[2] = (int (*)[2]) p;
-	
-	printf("%d\n", p[3]); // Qué imprime esta lı́nea?
-	printf("%d\n", reshape[1][1]); // Qué imprime esta lı́nea?
+void contarHasta(int hasta)
+{
+	for (int i = 0; i < hasta; i++)
+	{
+		printf("%d\n", i);
+	}
+}
+int main(void)
+{
+	contarHasta(10);
 	return 0;
 }
