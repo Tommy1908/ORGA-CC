60826df4420fe555e9ade54f85c7aa342cddebc0
Tommy1908 <tommy.villafane3@gmail.com>
2025-09-14 14:43:04 -0300

    ejercicio 2.a

--- guia_asm/src/7 - Ejercicios integradores/ej2/ej2.asm
+++ guia_asm/src/7 - Ejercicios integradores/ej2/ej2.asm
@@ -9,12 +9,16 @@ FALSE EQU 0
 ; Marca un ejercicio como hecho
 TRUE  EQU 1
 
+FILA  EQU 255
+COLUMNA EQU 255
+SIZE_OF_PUNTERO EQU 8
+
 ; Marca el ejercicio 1A como hecho (`true`) o pendiente (`false`).
 ;
 ; Funciones a implementar:
 ;   - optimizar
 global EJERCICIO_2A_HECHO
-EJERCICIO_2A_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_2A_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ; Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
 ;
@@ -32,10 +36,10 @@ EJERCICIO_2C_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
-ATTACKUNIT_CLASE EQU ??
-ATTACKUNIT_COMBUSTIBLE EQU ??
-ATTACKUNIT_REFERENCES EQU ??
-ATTACKUNIT_SIZE EQU ??
+ATTACKUNIT_CLASE EQU 0
+ATTACKUNIT_COMBUSTIBLE EQU 12
+ATTACKUNIT_REFERENCES EQU 14
+ATTACKUNIT_SIZE EQU 16
 
 global optimizar
 optimizar:
@@ -43,9 +47,81 @@ optimizar:
 	; ubicación según la convención de llamada. Prestá atención a qué
 	; valores son de 64 bits y qué valores son de 32 bits o 8 bits.
 	;
-	; r/m64 = mapa_t           mapa
-	; r/m64 = attackunit_t*    compartida
-	; r/m64 = uint32_t*        fun_hash(attackunit_t*)
+	; RDI = mapa_t           mapa[255][255]
+	; RSI = attackunit_t*    compartida
+	; RDX = uint32_t*        fun_hash(attackunit_t*)
+	push RBP
+	mov RBP, RSP
+	push R12 ;rbp-8		MAPA
+	push R13 ;rbp-16	COMPARTIDA
+	push R14 ;rbp-24	FUNCION	
+	sub RSP, 24;
+
+	;prologo
+	mov QWORD [rbp - 32], 0 ;indice i
+	mov QWORD [rbp - 40], 0 ;indice j
+
+	mov R12, RDI ;mapa
+	mov R13, RSI ;comparida
+	mov R14, RDX ;funcion
+
+	mov RDI, RSI
+	call RDX
+	mov [rbp - 48], RAX ; la funcion de hash nos queda aca
+
+	.cicloI:
+	cmp QWORD [rbp - 32], FILA
+	JE .finOptimizar
+
+	.cicloJ:
+	cmp QWORD [rbp - 40], COLUMNA
+	JE .cambiarJ
+
+	;Calcular offset de acceso
+
+	mov R10, [rbp - 32]  ;indice i 
+	imul R10, R10, COLUMNA ;R10 := i*columnas 
+	add R10, [rbp - 40] ; i*columnas + j 
+
+	mov RDI, [R12 + R10 * SIZE_OF_PUNTERO] ;mapa R12
+
+	;Verificar Iguales o NULL
+	cmp RDI, R13 ;comparar con compartida
+	JE .siguenteJ
+	cmp RDI, 0
+	JE .siguenteJ
+
+	push RDI ; guardo el UNIT IJ
+	push R10
+	
+	call R14 ; FUNCION HASH
+
+	pop R10 ;recupero offset 
+	pop RDI ;recupero UNIT
+
+	cmp RAX, [rbp - 48] ; Ver si el hash es igual
+	jne .siguenteJ
+	;OPTIMIZAR!!
+	mov [R12 + R10 * SIZE_OF_PUNTERO], R13 ;mapa[i][j] = compartida;
+	inc QWORD [R13 + ATTACKUNIT_REFERENCES] ;compartida->references ++;
+
+	.siguenteJ:
+	inc QWORD [rbp - 40]
+	jmp .cicloJ
+
+	.cambiarJ:
+	mov QWORD [rbp - 40], 0 ;indice j = 0
+	inc QWORD [rbp - 32] ;indice i ++
+	jmp .cicloI
+
+	.finOptimizar:
+
+	add rsp, 24
+	pop R14
+	pop R13
+	pop R12
+	;epilogo
+	pop RBP
 	ret
 
 global contarCombustibleAsignado
--- guia_asm/src/7 - Ejercicios integradores/ej2/ej2.c
+++ guia_asm/src/7 - Ejercicios integradores/ej2/ej2.c
@@ -8,9 +8,9 @@
  * Marca el ejercicio 1A como hecho (`true`) o pendiente (`false`).
  *
  * Funciones a implementar:
- *   - es_indice_ordenado
+ *   - optimizar
  */
-bool EJERCICIO_2A_HECHO = false;
+bool EJERCICIO_2A_HECHO = true;
 
 /**
  * Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
@@ -32,6 +32,33 @@ bool EJERCICIO_2C_HECHO = false;
  * OPCIONAL: implementar en C
  */
 void optimizar(mapa_t mapa, attackunit_t* compartida, uint32_t (*fun_hash)(attackunit_t*)) {
+    //[row][columns]
+    uint8_t fila,columna;
+    fila = 255;
+    columna = 255;
+
+    uint32_t hashCompartida, hashIJ;
+    attackunit_t* unitIJ;
+    hashCompartida = fun_hash(compartida);
+
+    for(uint8_t i = 0; i < fila; i++){
+        for(uint8_t j = 0; j < columna; j++){
+
+            unitIJ = mapa[i][j];
+
+            if(unitIJ == compartida || unitIJ == 0){  //aparentemente puede haber nulls
+                continue;
+            }
+
+            hashIJ = fun_hash(unitIJ);
+            
+            if(hashCompartida == hashIJ){
+                mapa[i][j] = compartida;
+                compartida->references ++;
+            }
+        }
+    }
+
 }
 
 /**
