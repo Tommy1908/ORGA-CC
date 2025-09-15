0c32d701d8a736ddbbc20fce967857b983739ed7
Tommy1908_ <tommy.villafane3@gmail.com>
2025-09-15 18:58:14 -0300

    ej7 con error

--- guia_asm/src/7 - Ejercicios integradores/ej2/ej2.asm
+++ guia_asm/src/7 - Ejercicios integradores/ej2/ej2.asm
@@ -25,14 +25,14 @@ EJERCICIO_2A_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 ; Funciones a implementar:
 ;   - contarCombustibleAsignado
 global EJERCICIO_2B_HECHO
-EJERCICIO_2B_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_2B_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ; Marca el ejercicio 1C como hecho (`true`) o pendiente (`false`).
 ;
 ; Funciones a implementar:
 ;   - modificarUnidad
 global EJERCICIO_2C_HECHO
-EJERCICIO_2C_HECHO: db FALSE ; Cambiar por `TRUE` para correr los tests.
+EJERCICIO_2C_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
@@ -77,33 +77,33 @@ optimizar:
 	cmp QWORD [rbp - 40], COLUMNA
 	JE .cambiarJ
 
-	;Calcular offset de acceso
-
-	mov R10, [rbp - 32]  ;indice i 
-	imul R10, R10, COLUMNA ;R10 := i*columnas 
-	add R10, [rbp - 40] ; i*columnas + j 
-
-	mov RDI, [R12 + R10 * SIZE_OF_PUNTERO] ;mapa R12
-
-	;Verificar Iguales o NULL
-	cmp RDI, R13 ;comparar con compartida
-	JE .siguenteJ
-	cmp RDI, 0
-	JE .siguenteJ
-
-	push RDI ; guardo el UNIT IJ
-	push R10
+		;Calcular offset de acceso
 	
-	call R14 ; FUNCION HASH
-
-	pop R10 ;recupero offset 
-	pop RDI ;recupero UNIT
-
-	cmp RAX, [rbp - 48] ; Ver si el hash es igual
-	jne .siguenteJ
-	;OPTIMIZAR!!
-	mov [R12 + R10 * SIZE_OF_PUNTERO], R13 ;mapa[i][j] = compartida;
-	inc QWORD [R13 + ATTACKUNIT_REFERENCES] ;compartida->references ++;
+		mov R10, [rbp - 32]  ;indice i 
+		imul R10, R10, COLUMNA ;R10 := i*columnas 
+		add R10, [rbp - 40] ; i*columnas + j 
+	
+		mov RDI, [R12 + R10 * SIZE_OF_PUNTERO] ;mapa R12
+	
+		;Verificar Iguales o NULL
+		cmp RDI, R13 ;comparar con compartida
+		JE .siguenteJ
+		cmp RDI, 0
+		JE .siguenteJ
+	
+		push RDI ; guardo el UNIT IJ
+		push R10
+		
+		call R14 ; FUNCION HASH
+	
+		pop R10 ;recupero offset 
+		pop RDI ;recupero UNIT
+	
+		cmp RAX, [rbp - 48] ; Ver si el hash es igual
+		jne .siguenteJ
+		;OPTIMIZAR!!
+		mov [R12 + R10 * SIZE_OF_PUNTERO], R13 ;mapa[i][j] = compartida;
+		inc QWORD [R13 + ATTACKUNIT_REFERENCES] ;compartida->references ++;
 
 	.siguenteJ:
 	inc QWORD [rbp - 40]
@@ -126,14 +126,149 @@ optimizar:
 
 global contarCombustibleAsignado
 contarCombustibleAsignado:
-	; r/m64 = mapa_t           mapa
-	; r/m64 = uint16_t*        fun_combustible(char*)
+	; RDI = mapa_t           mapa
+	; RSI = uint16_t*        fun_combustible(char*)
+	push RBP
+	mov RBP, RSP
+	push R12 ;MAPA
+	push R13 ;fun_combustible
+	push R14 ;i
+	push R15 ;j
+	sub RSP, 16
+	;EN [RBP - 40] voy a ir acumulando el total
+
+	mov QWORD[RBP-40], 0
+	xor R14, R14 ;Preparacion indices
+	xor R15, R15 ;Preparacion indices
+	mov R12, RDI ;Mapa
+	mov R13, RSI ;Func
+
+	.cicloI:
+	cmp R14, FILA
+	JE .finContar
+
+	.cicloJ:
+	cmp R15, COLUMNA
+	JE .cambiarJ
+	;;;;;;;;;;;;;;
+
+	mov R10, R14
+	imul R10, R10, COLUMNA
+	add R10, R15
+	; R10 = I * COMUNAS + J
+	mov R11 , [R12 + R10 * SIZE_OF_PUNTERO] ; AttackUnit
+	;Ver que no sea null
+	cmp QWORD R11, 0
+	je .siguenteJ
+
+	lea RDI, [R11 + ATTACKUNIT_CLASE]
+	xor R9D, R9D ;limpio 32 bits porque vamos a devolver 32
+	mov R9W, [R11 + ATTACKUNIT_COMBUSTIBLE] ; Combustible
+	
+	push R9 ;lo guardo
+	sub RSP, 8
+	
+
+	call R13
+
+	add RSP, 8
+	pop R9
+
+	sub R9W, AX ;la diferencia
+	add WORD[RBP - 40], R9W
+
+	jmp .siguenteJ
+
+	;;;;;;;;;;;;;;
+	.siguenteJ:
+	inc QWORD R15
+	jmp .cicloJ
+
+	.cambiarJ:
+	mov QWORD R15, 0 ;indice j = 0
+	inc QWORD R14 ;indice i ++
+	jmp .cicloI
+
+	.finContar:
+	mov EAX, DWORD[RBP-40]
+
+	add rsp, 16
+	pop R15
+	pop R14
+	pop R13
+	pop R12
+	pop RBP
 	ret
 
 global modificarUnidad
 modificarUnidad:
-	; r/m64 = mapa_t           mapa
-	; r/m8  = uint8_t          x
-	; r/m8  = uint8_t          y
-	; r/m64 = void*            fun_modificar(attackunit_t*)
+	; RDI = mapa_t           mapa
+	; SIL  = uint8_t          x
+	; DL  = uint8_t          y
+	; RCX = void*            fun_modificar(attackunit_t*)
+	;;;;;;;;;;;;;;;;;;
+	push RBP
+	mov RBP, RSP
+	push R12 ;AU
+	push R13 ; Info para acceder al mapa
+	push R14 ; Mapa
+	push R15 ; FUN
+	;;;;;;;;;;;;;;;;;; Fin prologo
+	mov R15, RCX
+	;Conseguir au
+	movzx R10, SIL ;que extienda a 64
+	movzx R11, DL
+	imul R10, COLUMNA
+	add R10, R11 ;i * columnas + j
+	;Guardar este shifteo y el mapa
+	mov R13, R10 ; Lo guardo parar acceder luego al mapa
+	mov R14, RDI ; Mapa
+	mov R12, [RDI + R10 * SIZE_OF_PUNTERO] ; R12 = AU origina
+	;
+
+	;Ver si es null, fin
+	cmp R12,0
+	JE .finModif
+	;
+
+	;Ver la cantidad de referencias, si es 1 MODIFICAR y fin
+	mov R8, [R12 + ATTACKUNIT_REFERENCES]
+	cmp R8, 1
+
+	JE .finModif1unit
+	;
+
+	dec BYTE[R12 + ATTACKUNIT_REFERENCES] ;Restar 1 a las referencias
+
+	;Reservar memoria para el nuevo au
+	mov RDI, ATTACKUNIT_SIZE
+	call malloc
+	;Establecer los valores
+	mov BYTE [RAX + ATTACKUNIT_REFERENCES], 1
+	mov R8W, [R12 + ATTACKUNIT_COMBUSTIBLE]
+	mov [RAX + ATTACKUNIT_COMBUSTIBLE], R8W
+	;el array de 11 bytes, tiene 1 de padding ademas, que deberia estar reservado tmb
+	mov R10, [R12 + ATTACKUNIT_CLASE] ;8 bytes
+	mov R11D, [R12 + ATTACKUNIT_CLASE + 8]; los 4 bytes que faltan
+
+	mov [RAX + ATTACKUNIT_CLASE], R10
+	mov [RAX + ATTACKUNIT_CLASE + 8], R11D
+
+	mov [R14 + R13 * SIZE_OF_PUNTERO], RAX
+
+	mov RDI, RAX
+	call R15
+	jmp .finModif
+
+	.finModif1unit:
+	mov RDI, R12
+	call R15
+	.finModif:
+
+	;;;;;;;;;;;;;;;;;; Epilogo
+	pop R15
+	pop R14
+	pop R13
+	pop R12
+	pop RBP
 	ret
--- guia_asm/src/7 - Ejercicios integradores/ej2/ej2.c
+++ guia_asm/src/7 - Ejercicios integradores/ej2/ej2.c
@@ -18,7 +18,7 @@ bool EJERCICIO_2A_HECHO = true;
  * Funciones a implementar:
  *   - contarCombustibleAsignado
  */
-bool EJERCICIO_2B_HECHO = false;
+bool EJERCICIO_2B_HECHO = true;
 
 /**
  * Marca el ejercicio 1B como hecho (`true`) o pendiente (`false`).
@@ -26,49 +26,109 @@ bool EJERCICIO_2B_HECHO = false;
  * Funciones a implementar:
  *   - modificarUnidad
  */
-bool EJERCICIO_2C_HECHO = false;
+bool EJERCICIO_2C_HECHO = true;
 
 /**
  * OPCIONAL: implementar en C
  */
-void optimizar(mapa_t mapa, attackunit_t* compartida, uint32_t (*fun_hash)(attackunit_t*)) {
+void optimizar(mapa_t mapa, attackunit_t *compartida, uint32_t (*fun_hash)(attackunit_t *))
+{
     //[row][columns]
-    uint8_t fila,columna;
+    uint8_t fila, columna;
     fila = 255;
     columna = 255;
 
     uint32_t hashCompartida, hashIJ;
-    attackunit_t* unitIJ;
+    attackunit_t *unitIJ;
     hashCompartida = fun_hash(compartida);
 
-    for(uint8_t i = 0; i < fila; i++){
-        for(uint8_t j = 0; j < columna; j++){
+    for (uint8_t i = 0; i < fila; i++)
+    {
+        for (uint8_t j = 0; j < columna; j++)
+        {
 
             unitIJ = mapa[i][j];
 
-            if(unitIJ == compartida || unitIJ == 0){  //aparentemente puede haber nulls
+            if (unitIJ == compartida || unitIJ == 0)
+            { // aparentemente puede haber nulls
                 continue;
             }
 
             hashIJ = fun_hash(unitIJ);
-            
-            if(hashCompartida == hashIJ){
+
+            if (hashCompartida == hashIJ)
+            {
                 mapa[i][j] = compartida;
-                compartida->references ++;
+                compartida->references++;
+                if (unitIJ->references == 1)
+                {
+                    free(unitIJ);
+                }
+                else
+                {
+                    unitIJ->references--;
+                }
             }
         }
     }
-
 }
 
 /**
  * OPCIONAL: implementar en C
  */
-uint32_t contarCombustibleAsignado(mapa_t mapa, uint16_t (*fun_combustible)(char*)) {
+uint32_t contarCombustibleAsignado(mapa_t mapa, uint16_t (*fun_combustible)(char *))
+{
+    uint8_t fila, columna;
+    fila = 255;
+    columna = 255;
+    uint32_t total = 0;
+
+    for (uint8_t i = 0; i < fila; i++)
+    {
+        for (uint8_t j = 0; j < columna; j++)
+        {
+            attackunit_t *au = mapa[i][j];
+            if (au == 0)
+            {
+                continue;
+            }
+            char *clase = au->clase;
+            uint16_t combustible_base = fun_combustible(clase);
+            uint16_t combustible_total = au->combustible;
+            total += combustible_total - combustible_base;
+        }
+    }
+    return total;
 }
 
 /**
  * OPCIONAL: implementar en C
  */
-void modificarUnidad(mapa_t mapa, uint8_t x, uint8_t y, void (*fun_modificar)(attackunit_t*)) {
+void modificarUnidad(mapa_t mapa, uint8_t x, uint8_t y, void (*fun_modificar)(attackunit_t *))
+{
+
+    attackunit_t *au = mapa[x][y];
+    if (au == 0)
+    {
+        return;
+    }
+    if (au->references == 1)
+    {
+        fun_modificar(au);
+        return;
+    }
+
+    au->references--;
+
+    attackunit_t *mod_au = (attackunit_t *)malloc(sizeof(attackunit_t));
+    mod_au->combustible = au->combustible;
+    mod_au->references = 1;
+    strncpy(mod_au->clase, au->clase, 11);
+    // mod_au->clase = au->clase; esto no anda porque es un array no un puntero
+
+    mapa[x][y] = mod_au;
+
+    fun_modificar(mod_au);
+
+    return;
 }
